9984f2d31bd5a95790dc94ca9e8c4a3f1a4162f5
Alex <alex.wallerand@gmail.com>
2022-05-10 22:42:02 +0200

    Add comments, better tests and better a constructor for the HuffmanTree

--- src/HuffmanTree.java
+++ src/HuffmanTree.java
@@ -1,23 +1,29 @@
-import java.util.ArrayList;
-import java.util.LinkedHashMap;
-import java.util.Set;
-import java.util.ArrayList;
-import java.util.Comparator;
+import java.util.ArrayList; // Import the ArrayList class for the nodes list
+import java.util.LinkedHashMap; // Import the LinkedHashMap class for the frequencies dictionary
+import java.util.Set; // Import the Set class to have a set of all the keys of the dictionary
+import java.util.Comparator; // Import the Comparator class to sort the nodes list
 
 public class HuffmanTree {
+    /**
+     * Class to create the Huffman tree, which contains a list of nodes of the Node class
+     */
 
     private ArrayList<Node> nodes;
 
-    public HuffmanTree(LinkedHashMap<String,Integer> freq){
+    public HuffmanTree(LinkedHashMap<String, Integer> freq) {
+        /**
+         * Constructor of the HuffmanTree class
+         * @param freq, dictionary of frequencies
+         */
 
         nodes = new ArrayList<>();
         Set<String> keys = freq.keySet();
 
-        for(String key : keys){
-            nodes.add(new Node(key,freq.get(key)));
+        for (String key : keys) {
+            nodes.add(new Node(key, freq.get(key)));
         }
 
-        while(nodes.size() > 1){
+        while (nodes.size() > 1) {
             nodes.sort(Comparator.comparing(Node::getFrequency).thenComparing(Node::getLabel));
 
             Node left = nodes.get(0);
@@ -29,14 +35,16 @@ public class HuffmanTree {
             right.setBranch("1");
 
             int sum = left.getFrequency() + right.getFrequency();
-            Node newNode = new Node(Integer.toString(sum),sum,left,right);
+            Node newNode = new Node(Integer.toString(sum), sum, left, right);
 
             nodes.add(newNode);
         }
-        this.nodes = nodes;
     }
 
     public ArrayList<Node> getNodes() {
+        /**
+         * Getter of the nodes attribute
+         */
         return nodes;
     }
 }
--- src/Tests.java
+++ src/Tests.java
@@ -1,22 +1,14 @@
-import java.io.FileNotFoundException;
-import java.util.Comparator;
+import java.io.IOException;
 import java.util.LinkedHashMap;
-import java.util.PriorityQueue;
-import java.util.ArrayList;
 
 public class Tests {
 
-    public static void main(String[] args) throws FileNotFoundException {
-        /*FreqFile file = new FreqFile("exemple");
+    public static void main(String[] args) throws IOException {
+
+        FreqFile file = new FreqFile("exemple");
         int nb_characters = file.getNumberOfCharacters();
-        LinkedHashMap<String,Integer> freq = file.read();
-        System.out.println(freq);*/
-        LinkedHashMap d = new LinkedHashMap<>();
-        d.put("b",1);
-        d.put("c",6);
-        d.put("a",5);
-        d.put("d",3);
-        HuffmanTree tree = new HuffmanTree(d);
+        LinkedHashMap<String, Integer> freq = file.getFreqDict();
+        HuffmanTree tree = new HuffmanTree(freq);
         System.out.println(tree.getNodes());
     }
 }
