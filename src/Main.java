7d1b164cccd906fd549c0f824069c1495bc1f68c
Alex <alex.wallerand@gmail.com>
2022-05-12 15:20:03 +0200

    Now handles the program arguments

--- src/Main.java
+++ src/Main.java
@@ -4,29 +4,33 @@ import java.util.LinkedHashMap;
 public class Main {
 
     public static void main(String[] args) throws IOException {
+        for (String arg : args) {
 
-        // Read the frequency file and get the data into a map
-        FreqFile file = new FreqFile("alice");
-        int nb_characters = file.getNumberOfCharacters();
-        LinkedHashMap<String, Integer> freq = file.getFreqDict();
+            System.out.println("\nStarting the decompression of " + arg);
 
-        // Build the HuffmanTree with the frequency map
-        HuffmanTree tree = new HuffmanTree(freq);
+            // Read the frequency file and get the data into a map
+            FreqFile file = new FreqFile(arg);
+            int nb_characters = file.getNumberOfCharacters();
+            LinkedHashMap<String, Integer> freq = file.getFreqDict();
 
-        // Read the binary file to convert the bytes to a binary string
-        BinFile f = new BinFile("alice");
-        String bin = f.getBinToStr();
+            // Build the HuffmanTree with the frequency map
+            HuffmanTree tree = new HuffmanTree(freq);
 
-        // Decode the binary string by coursing the HuffmanTree
-        DecodeResults res = tree.decode(bin);
-        String text = res.getText();
-        float average = res.getAverageNumberOfBits();
+            // Read the binary file to convert the bytes to a binary string
+            BinFile f = new BinFile(arg);
+            String bin = f.getBinToStr();
 
-        // Generate the results in a text file
-        DecompFile output = new DecompFile("alice");
-        output.write_text(text);
-        CompressRate compressRate = new CompressRate("alice");
-        System.out.printf("Average bits per character: " + "%.2f%n", average);
-        compressRate.calculate();
+            // Decode the binary string by coursing the HuffmanTree
+            DecodeResults res = tree.decode(bin);
+            String text = res.getText();
+            float average = res.getAverageNumberOfBits();
+
+            // Generate the results in a text file
+            DecompFile output = new DecompFile(arg);
+            output.write_text(text);
+            CompressRate compressRate = new CompressRate(arg);
+            System.out.printf("Average bits per character: " + "%.2f%n", average);
+            compressRate.calculate();
+        }
     }
 }
